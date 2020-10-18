2a20e0efaa136f120dccea30bfed79f7f13ddd7a
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-19 00:28:52 +0500

    fix: expand errorHandler

--- middleware/celebrateValidation/errorHandler.js
+++ middleware/celebrateValidation/errorHandler.js
@@ -3,9 +3,14 @@
 const { isCelebrateError } = require('celebrate');
 
 module.exports = (err, req, res, next) => {
-  if (isCelebrateError(err)) {
+  if (isCelebrateError(err) && err.details.get('body')) {
     return res.status(400).send({
-      message: err.details.get('body').message.replace(/['"]/g, ''),
+      message: err.details.get('body').message.replace(/"/g, ''),
+    });
+  }
+  if (isCelebrateError(err) && err.details.get('params')) {
+    return res.status(400).send({
+      message: err.details.get('params').message.replace(/"/g, ''),
     });
   }
   return next(err);
