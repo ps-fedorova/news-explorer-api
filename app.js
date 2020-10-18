6eff4094f3b744a299dcd4fff83c9b0385ab7874
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-19 04:28:41 +0500

    refactor: transfer errorHandler

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 const celebrateErrorHandler = require('./middleware/celebrateValidation/celebrateErrorHandler'); // кастомизированная обработка ошибок
 
 const { NotFoundError } = require('./errors');
-
+const errorHandler = require('./middleware/errorHandler');
 const { limiter } = require('./middleware/expressRateLimit');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const router = require('./routes');
@@ -37,12 +37,6 @@ app.use(limiter); // для ограничения количества запр
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: false })); // для приёма веб-страниц внутри POST-запроса
 
-app.get('/crash-test', () => { // краш-тест сервера
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(router); // обработчик роутов
 
 // errorLogger нужно подключить после обработчиков роутов и до обработчиков ошибок
@@ -55,15 +49,6 @@ app.use(() => {
   throw new NotFoundError({ message: CLIENT_ERROR.RESOURCE_NOT_FOUND });
 });
 
-app.use((err, req, res, next) => {
-  if (err.status !== '500') {
-    res.status(err.status)
-      .send(err.message);
-    return;
-  }
-  res.status(500)
-    .send({ message: `${SERVER_ERROR.INTERNAL_SERVER_ERROR}: ${err.message}` });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT);
