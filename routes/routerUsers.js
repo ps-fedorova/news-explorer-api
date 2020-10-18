5c18f817f9f8baa3fdfb2945084b7bf99c4145a2
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-18 21:07:37 +0500

    fix: delete validateId from routerUsers

--- routes/routerUsers.js
+++ routes/routerUsers.js
@@ -1,7 +1,6 @@
 const usersRouter = require('express').Router();
-const { validateId } = require('../middleware/celebrateValidation/celebrateValidation');
 const { getCurrentUser } = require('../controllers/controllersUsers');
 
-usersRouter.get('/me', validateId, getCurrentUser);
+usersRouter.get('/me', getCurrentUser);
 
 module.exports = usersRouter;
