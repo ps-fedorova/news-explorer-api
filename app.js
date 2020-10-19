9c6165fa74aa6270d7ca76f51f147b33f82731e4
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-19 07:37:15 +0500

    refactor: move NotFound

--- app.js
+++ app.js
@@ -8,16 +8,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 // const { errors } = require('celebrate'); // кастомизированная обработка ошибок (↓)
 const celebrateErrorHandler = require('./middleware/celebrateValidation/celebrateErrorHandler');
-const { NotFoundError } = require('./errors');
 const errorHandler = require('./middleware/errorHandler');
 const { limiter } = require('./middleware/expressRateLimit');
 const { requestLogger, errorLogger } = require('./middleware/logger');
-const router = require('./routes');
-const { CLIENT_ERROR } = require('./libs/statusMessages');
 const { PORT, MONGO } = require('./config');
+const router = require('./routes');
 
 const app = express();
-app.use(cookieParser());
 
 mongoose.connect(MONGO, { // подключение БД
   useNewUrlParser: true,
@@ -26,11 +23,11 @@ mongoose.connect(MONGO, { // подключение БД
   useUnifiedTopology: true,
 });
 
-app.use(helmet()); // для простановки security-заголовков для API
 app.use(limiter); // для ограничения количества запросов (до 100 раз за 15 минут)
+app.use(helmet()); // для простановки security-заголовков для API
 
+app.use(cookieParser());
 app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: false })); // для приёма веб-страниц внутри POST-запроса
 
 app.use(requestLogger); // логгер запросов нужно подключить до всех обработчиков роутов
 
@@ -41,10 +38,6 @@ app.use(errorLogger);
 
 // app.use(errors()); // обработка ошибок в из celebrate
 app.use(celebrateErrorHandler);
-
-app.use(() => { // "не найдено"
-  throw new NotFoundError({ message: CLIENT_ERROR.RESOURCE_NOT_FOUND });
-});
 
 app.use(errorHandler); // "ошибка сервера"
 
