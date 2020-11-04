5bb2cf1260aa9484316861353a34126f2b73c638
Polina Fedorova <ps.fedorova@yandex.ru>
2020-11-05 04:30:46 +0500

    fix: fix error handling

--- middleware/checkPassword.js
+++ middleware/checkPassword.js
@@ -1,12 +1,12 @@
 const { checkPassword } = require('../libs/validationMessages');
+const { BadRequestError } = require('../errors');
 
 module.exports = (req, res, next) => {
   const { password } = req.body;
   const regex = /^\S*$/;
 
   if (!password || !password.match(regex)) {
-    res.status(400)
-      .send({ message: checkPassword });
+    next(new BadRequestError({ message: checkPassword }));
   } else {
     next();
   }
